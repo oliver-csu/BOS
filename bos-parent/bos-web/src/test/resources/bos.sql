5ae569dc72f990a243f2d46c112991bd2e472609
oliver-csu <oliver.csu@gmail.com>
2019-04-20 21:28:07 +0800

    基础数据补充

--- bos-parent/bos-web/src/test/resources/bos.sql
+++ bos-parent/bos-web/src/test/resources/bos.sql
@@ -1,146 +1,28 @@
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-  `id` varchar(32) NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `salary` double DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  `gender` varchar(10) DEFAULT NULL,
-  `station` varchar(40) DEFAULT NULL,
-  `telephone` varchar(11) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 INSERT INTO `t_user` VALUES ('1', 'oliver', '81dc9bdb52d04dc20036dbd8313ed055', null, null, null, null, null, null);
-
-DROP TABLE IF EXISTS `bc_staff`;
-CREATE TABLE `bc_staff` (
-  `id` varchar(32) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `telephone` varchar(20) DEFAULT NULL,
-  `haspda` char(1) DEFAULT NULL,
-  `deltag` char(1) DEFAULT NULL,
-  `station` varchar(40) DEFAULT NULL,
-  `standard` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `bc_staff` VALUES ('402840815bf0da5b015bf0db0b370000', '张三', '15705102215', '1', '0', '苏州分部', '10-20公斤');
 INSERT INTO `bc_staff` VALUES ('402840815c01bcaf015c01be0f540000', '李四', '15705102215', '1', '0', '北京分部', '10-20公斤');
 
-DROP TABLE IF EXISTS `bc_region`;
-CREATE TABLE `bc_region` (
-  `id` varchar(32) NOT NULL,
-  `province` varchar(50) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `district` varchar(50) DEFAULT NULL,
-  `postcode` varchar(50) DEFAULT NULL,
-  `shortcode` varchar(30) DEFAULT NULL,
-  `citycode` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `bc_decidedzone`;
-CREATE TABLE `bc_decidedzone` (
-  `id` varchar(32) NOT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `staff_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKh0xplk12o52a6eryw4hcqnfwt` (`staff_id`),
-  CONSTRAINT `FK_decidedzone_staff` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`),
-  CONSTRAINT `FKh0xplk12o52a6eryw4hcqnfwt` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `bc_subarea`;
-CREATE TABLE `bc_subarea` (
-  `id` varchar(32) NOT NULL,
-  `decidedzone_id` varchar(32) DEFAULT NULL,
-  `region_id` varchar(32) DEFAULT NULL,
-  `addresskey` varchar(100) DEFAULT NULL,
-  `startnum` varchar(30) DEFAULT NULL,
-  `endnum` varchar(30) DEFAULT NULL,
-  `single` char(1) DEFAULT NULL,
-  `position` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKlndbc8oldgbg3k1x63n3n6t7n` (`decidedzone_id`),
-  KEY `FKcjwxm19mx5njn3xyqgqp431mb` (`region_id`),
-  CONSTRAINT `FK_area_decidedzone` FOREIGN KEY (`decidedzone_id`) REFERENCES `bc_decidedzone` (`id`),
-  CONSTRAINT `FK_area_region` FOREIGN KEY (`region_id`) REFERENCES `bc_region` (`id`),
-  CONSTRAINT `FKcjwxm19mx5njn3xyqgqp431mb` FOREIGN KEY (`region_id`) REFERENCES `bc_region` (`id`),
-  CONSTRAINT `FKlndbc8oldgbg3k1x63n3n6t7n` FOREIGN KEY (`decidedzone_id`) REFERENCES `bc_decidedzone` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 INSERT INTO `bc_decidedzone` VALUES ('123', '朝阳', '402840815c01bcaf015c01be0f540000');
+
 INSERT INTO `bc_subarea` VALUES ('402840815c05a29f015c05a39bd80000', '123', 'QY003', '朝阳区', '1', '100', '0', '朝阳大道');
 INSERT INTO `bc_subarea` VALUES ('402840815c14d135015c14d633560000', '123', 'QY004', '丰台区', '1', '100', '0', '丰台区');
 
-DROP TABLE IF EXISTS `qp_noticebill`;
-CREATE TABLE `qp_noticebill` (
-  `id` varchar(32) NOT NULL,
-  `staff_id` varchar(32) DEFAULT NULL,
-  `customer_id` varchar(32) DEFAULT NULL,
-  `customer_name` varchar(20) DEFAULT NULL,
-  `delegater` varchar(20) DEFAULT NULL,
-  `telephone` varchar(20) DEFAULT NULL,
-  `pickaddress` varchar(200) DEFAULT NULL,
-  `arrivecity` varchar(20) DEFAULT NULL,
-  `product` varchar(20) DEFAULT NULL,
-  `pickdate` date DEFAULT NULL,
-  `num` int(11) DEFAULT NULL,
-  `weight` double DEFAULT NULL,
-  `volume` varchar(20) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `ordertype` varchar(20) DEFAULT NULL,
-  `user_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKl5j3pm969oy1qdc1318jrmbgt` (`user_id`),
-  KEY `FKhmbqr6qlg0uets978w5xshler` (`staff_id`),
-  CONSTRAINT `FK_Reference_3` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`),
-  CONSTRAINT `FKhmbqr6qlg0uets978w5xshler` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`),
-  CONSTRAINT `FKl5j3pm969oy1qdc1318jrmbgt` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`),
-  CONSTRAINT `test` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `qp_workbill`;
-CREATE TABLE `qp_workbill` (
-  `id` varchar(32) NOT NULL,
-  `noticebill_id` varchar(32) DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  `pickstate` varchar(20) DEFAULT NULL,
-  `buildtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `attachbilltimes` int(11) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  `staff_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKggojlcovnpimuukxcueb18apt` (`noticebill_id`),
-  KEY `FK55ckcnjyvwinnnniu5jpelg7y` (`staff_id`),
-  CONSTRAINT `FK55ckcnjyvwinnnniu5jpelg7y` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`),
-  CONSTRAINT `FK_Reference_4` FOREIGN KEY (`staff_id`) REFERENCES `bc_staff` (`id`),
-  CONSTRAINT `FK_workbill_noticebill_fk` FOREIGN KEY (`noticebill_id`) REFERENCES `qp_noticebill` (`id`),
-  CONSTRAINT `FKggojlcovnpimuukxcueb18apt` FOREIGN KEY (`noticebill_id`) REFERENCES `qp_noticebill` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `qp_workordermanage`;
-CREATE TABLE `qp_workordermanage` (
-  `id` varchar(32) NOT NULL,
-  `arrivecity` varchar(20) DEFAULT NULL,
-  `product` varchar(20) DEFAULT NULL,
-  `num` int(11) DEFAULT NULL,
-  `weight` double DEFAULT NULL,
-  `floadreqr` varchar(255) DEFAULT NULL,
-  `prodtimelimit` varchar(40) DEFAULT NULL,
-  `prodtype` varchar(40) DEFAULT NULL,
-  `sendername` varchar(20) DEFAULT NULL,
-  `senderphone` varchar(20) DEFAULT NULL,
-  `senderaddr` varchar(200) DEFAULT NULL,
-  `receivername` varchar(20) DEFAULT NULL,
-  `receiverphone` varchar(20) DEFAULT NULL,
-  `receiveraddr` varchar(200) DEFAULT NULL,
-  `feeitemnum` int(11) DEFAULT NULL,
-  `actlweit` double DEFAULT NULL,
-  `vol` varchar(20) DEFAULT NULL,
-  `managerCheck` varchar(1) DEFAULT NULL,
-  `updatetime` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `auth_function` VALUES ('11', NULL, '基础档案', 'jichudangan', NULL, NULL, '1', 0);
+INSERT INTO `auth_function` VALUES ('112', '11', '收派标准', 'standard', NULL, 'page_base_standard.action', '1', 1);
+INSERT INTO `auth_function` VALUES ('113', '11', '取派员设置', 'staff', NULL, 'page_base_staff.action', '1', 2);
+INSERT INTO `auth_function` VALUES ('114', '11', '区域设置', 'region', NULL, 'page_base_region.action', '1', 3);
+INSERT INTO `auth_function` VALUES ('115', '11', '管理分区', 'subarea', NULL, 'page_base_subarea.action', '1', 4);
+INSERT INTO `auth_function` VALUES ('116', '11', '管理定区/调度排班', 'decidedzone', NULL, 'page_base_decidedzone.action', '1', 5);
+INSERT INTO `auth_function` VALUES ('12', NULL, '受理', 'shouli', NULL, NULL, '1', 1);
+INSERT INTO `auth_function` VALUES ('121', '12', '业务受理', 'noticebill', NULL, 'page_qupai_noticebill_add.action', '1', 0);
+INSERT INTO `auth_function` VALUES ('122', '12', '工作单快速录入', 'quickworkordermanage', NULL, 'page_qupai_quickworkorder.action', '1', 1);
+INSERT INTO `auth_function` VALUES ('124', '12', '工作单导入', 'workordermanageimport', NULL, 'page_qupai_workorderimport.action', '1', 3);
+INSERT INTO `auth_function` VALUES ('13', NULL, '调度', 'diaodu', NULL, NULL, '1', 2);
+INSERT INTO `auth_function` VALUES ('131', '13', '查台转单', 'changestaff', NULL, NULL, '1', 0);
+INSERT INTO `auth_function` VALUES ('132', '13', '人工调度', 'personalassign', NULL, 'page_qupai_diaodu.action', '1', 1);
+INSERT INTO `auth_function` VALUES ('14', NULL, '物流配送流程管理', 'zhongzhuan', NULL, NULL, '1', 3);
+INSERT INTO `auth_function` VALUES ('141', '14', '启动配送流程', 'start', NULL, 'workOrderManageAction_list.action', '1', 0);
+INSERT INTO `auth_function` VALUES ('142', '14', '查看个人任务', 'personaltask', NULL, 'taskAction_findPersonalTask.action', '1', 1);
+INSERT INTO `auth_function` VALUES ('143', '14', '查看我的组任务', 'grouptask', NULL, 'taskAction_findGroupTask.action', '1', 2);
+INSERT INTO `auth_function` VALUES ('144', '113', '删除取派员', 'staff.delete', 'xxx', 'staffAction_delete.action', '0', 1);
